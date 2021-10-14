e00365eff0ee180573c8c0f445aaf8f3f6bc8975
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-14 08:32:44 -0400

    Psudocode and define initial variables.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,34 @@
-// Assignment Code
+//Psudocode
+
+//assign code to button to generate a password
+//create prompts/confirms to collect criteria for the password
+    var confirmLower = confirm("Do you want to use lower case letters?")
+    var confirmUpper = confirm("Do you want to use upper case letters?")
+    var confirmNum = confirm("Do you want to use a number?")
+    var confirmSpecial = confirm("Do you want to use a special character?")
+    var promptLength = prompt("Enter Number of characters between 8-128.")
+//use the data from those prompts to assign to variables
+    // Validate input
+    //Select at least one char type each
+        // Length of pwd: 8-128
+        //Char types
+            //lowercase
+            //uppercase
+            //numeric
+            //special
+//Once all prompts answered, generate password in <textarea>  (or alert)
+
+//Arrays for charTypes
+
+var charLower=[]
+var charUpper=[]
+var charNum=[]
+var charSpecial=[]
+
+
+//code below was given
+
+// button variable assigned by selcecting the ID for the button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
