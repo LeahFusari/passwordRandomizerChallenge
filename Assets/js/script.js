b23028079c15df309ccc33d1b0135d60933186d4
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-15 15:50:07 -0400

    cleaned up comments and inserted for loop

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,61 +1,8 @@
-//Psudocode
 
-//assign code to button to generate a password
-//create prompts/confirms to collect criteria for the password
-// var promptLength = prompt("Enter Number of characters between 8-128.")    
-// var confirmLower = confirm("Do you want to use lower case letters?")
-// var confirmUpper = confirm("Do you want to use upper case letters?")
-// var confirmNum = confirm("Do you want to use a number?")
-// var confirmSpecial = confirm("Do you want to use a special character?")
-
-// //use the data from those prompts to assign to variables
-// Validate input
-//Select at least one char type each
-// Length of pwd: 8-128
-//Char types
-//lowercase
-//uppercase
-//numeric
-//special
-//Once all prompts answered, generate password in <textarea>  (or alert)
-
-//Arrays for charTypes
-
-// function getRandomUpper(){
-//     return String.fromCharCode(Math.floor(Math.random()*26)+65);
-
-// }
-
-// for (var i = 0; i < randArray.length;i++) {
-//     console.log(randArray[i]);
-// }
-
-//series of prompts
-
-
-
-// }
-// console.log(randUpper,randLower,randNumber,randSpecial);
-
-// for (var i = 0; i < randUpper.length; i++) {
-//     console.log(randUpper[i]);
-// }
-
-// for (var i = 0; i < randLower.length; i++) {
-//     console.log(randLower[i]);
-// }
-
-// for (var i = 0; i < randNumber.length; i++) {
-//     console.log(randNumber[i]);
-// }
-
-// for (var i = 0; i < randSpecial.length; i++) {
-//     console.log(randSpecial[i]);
-// }
-var alphaUpper = ["A", "B", "C"]
-var alphaLower = ["a", "b", "c"]
-var alphaSpecial = ["!", "@", "#"]
-var intNumber = [1, 2, 3]
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var alphaSpecial = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"]
+var intNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 // button variable assigned by selcecting the ID for the button
@@ -96,11 +43,10 @@ function generatePassword() {
     console.log(allPossible);
 
     //use for loop to randomly get the elements from the allPossible array as many times as the user selects.
-
-    // grab the random and save to another variable finalPassord
-    // retrurn final password
-//return that array
-
+    for (var i = 0; i < promptLength; i++) {
+        finalPassword = allPossible[Math.floor(Math.random()*allPossible.length)];
+      }
+      return finalPassword;
 }
 
 
@@ -116,4 +62,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Validation
