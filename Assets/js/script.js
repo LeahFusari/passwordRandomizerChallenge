2384a0f368eadb58c5b969cd17a21a70449a5b60
LeahFusari <87339742+LeahFusari@users.noreply.github.com>
2021-10-16 11:39:35 -0400

    Completed validation and reset button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,22 +8,27 @@ var intNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 // button variable assigned by selcecting the ID for the button
 var generateBtn = document.querySelector("#generate");
 var resetBtn = document.querySelector("#reset");
-var textarea = document.querySelector("#password");
 
 function generatePassword() {
     var promptLength = prompt("Enter Number of characters between 8-128.");
     var finalPassword = ""
 
-    if (promptLength < 8 || promptLength > 128 ) {
+    if (promptLength < 8 || promptLength > 128) {
         alert("You must choose a number between 8 and 128");
-        document.querySelector("#password").reset();
         return
     }
+
+     if(isNaN(promptLength)){
+            alert("You must choose a number between 8 and 128");
+            return
+        }
     
-    var confirmLower = confirm("Do you want to use lower case letters?");
-    var confirmUpper = confirm("Do you want to use upper case letters?");
-    var confirmNumber = confirm("Do you want to use a number?");
-    var confirmSpecial = confirm("Do you want to use a special character?");
+    var confirmLower = confirm("Do you want to use lower case letters?  Click 'OK' for Yes and 'Cancel' for No.");
+    var confirmUpper = confirm("Do you want to use upper case letters? Click 'OK' for Yes and 'Cancel' for No.");
+    var confirmNumber = confirm("Do you want to use a number? Click 'OK' for Yes and 'Cancel' for No." );
+    var confirmSpecial = confirm("Do you want to use a special character? Click 'OK' for Yes and 'Cancel' for No.");
+
+    
 
     var allPossible =[]
 
@@ -45,7 +50,7 @@ function generatePassword() {
 
     if (!confirmLower && !confirmUpper && !confirmSpecial && !confirmNumber) {
         alert("Pleaes choose at least one character option!");
-        document.querySelector("#password").reset();
+        
     return
     }
 
@@ -61,15 +66,15 @@ function generatePassword() {
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    
+        if(password==="undefined"){
+            password=" "
+        }
     passwordText.value = password;
     return
 }
 
 function clearTextArea(){
-    var passwordText = document.querySelector("#password");
-
-    passwordText = "xyz";
+    location.reload()
 }
 
 // Add event listener to generate button
